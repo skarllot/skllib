0d2f883939717a2ef1e37a93f442637d51bd0b25
Fabrício Godoy <fgodoy@rodrimar.com.br>
2013-11-06 11:30:13 -0200

    Removed "SklLib.Forms" project
    The project "SklLib.Forms" was moved to skllib-forms repository.
    Changed README file to fit current state.

--- SklLib/AssemblyInfo.cs
+++ SklLib/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 // Latest release: 0.1.35.0
 // Major.Minor.Maintenance.Revision
-[assembly: AssemblyVersion("0.2.0.42")]
-[assembly: AssemblyFileVersionAttribute("0.2.0.42")]
+[assembly: AssemblyVersion("0.2.0.43")]
+[assembly: AssemblyFileVersionAttribute("0.2.0.43")]
