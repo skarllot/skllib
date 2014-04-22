23eb2baa29feb83165b42e0d08a71698ad1552cf
Fabricio Godoy <skarllot@hotmail.com>
2014-04-22 01:37:07 -0300

    Created new class to get assembly custom attributes

--- SklLib/Environment/AssemblyInfo.cs
+++ SklLib/Environment/AssemblyInfo.cs
@@ -19,6 +19,7 @@
 //
 
 using System;
+using System.Reflection;
 
 namespace SklLib.Environment
 {
@@ -27,5 +28,87 @@ namespace SklLib.Environment
     /// </summary>
     public static class AssemblyInfo
     {
+        #region Fields
+
+        static readonly string company;
+        static readonly string version;
+        static readonly string product;
+        static readonly string copyright;
+        static readonly string title;
+        static readonly string description;
+
+        #endregion
+
+        #region Constructors
+
+        static AssemblyInfo()
+        {
+            Assembly currentAssembly = Assembly.GetEntryAssembly();
+            if (currentAssembly == null)
+                return;
+
+            AssemblyCompanyAttribute aCompany = GetCustomAttribute<AssemblyCompanyAttribute>(currentAssembly);
+            if (aCompany != null)
+                company = aCompany.Company;
+            AssemblyVersionAttribute aVersion = GetCustomAttribute<AssemblyVersionAttribute>(currentAssembly);
+            if (aVersion != null)
+                version = aVersion.Version;
+            AssemblyProductAttribute aProduct = GetCustomAttribute<AssemblyProductAttribute>(currentAssembly);
+            if (aProduct != null)
+                product = aProduct.Product;
+            AssemblyCopyrightAttribute aCopyright = GetCustomAttribute<AssemblyCopyrightAttribute>(currentAssembly);
+            if (aCopyright != null)
+                copyright = aCopyright.Copyright;
+            AssemblyTitleAttribute aTitle = GetCustomAttribute<AssemblyTitleAttribute>(currentAssembly);
+            if (aTitle != null)
+                title = aTitle.Title;
+            AssemblyDescriptionAttribute aDescription = GetCustomAttribute<AssemblyDescriptionAttribute>(currentAssembly);
+            if (aDescription != null)
+                description = aDescription.Description;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the company property from assembly information.
+        /// </summary>
+        public static string Company { get { return company; } }
+        /// <summary>
+        /// Gets the version property from assembly information.
+        /// </summary>
+        public static string Version { get { return version; } }
+        /// <summary>
+        /// Gets the product property from assembly information.
+        /// </summary>
+        public static string Product { get { return product; } }
+        /// <summary>
+        /// Gets the copyright property from assembly information.
+        /// </summary>
+        public static string Copyright { get { return copyright; } }
+        /// <summary>
+        /// Gets the title property from assembly information.
+        /// </summary>
+        public static string Title { get { return title; } }
+        /// <summary>
+        /// Gets the description property from assembly information.
+        /// </summary>
+        public static string Description { get { return description; } }
+
+        #endregion
+
+        #region Methods
+
+        static T GetCustomAttribute<T>(Assembly assembly) where T : class
+        {
+            object[] cAttrib = assembly.GetCustomAttributes(typeof(T), false);
+            if (cAttrib == null || cAttrib.Length == 0)
+                return null;
+
+            return cAttrib[0] as T;
+        }
+
+        #endregion
     }
 }
