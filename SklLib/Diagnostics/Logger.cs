f8411685800b33a0d3fc29132e6eb1a3a3772f89
Fabrício Godoy <fgodoy@rodrimar.com.br>
2014-10-15 19:17:43 -0300

    New class to handle Windows EventLog

---
+++ SklLib/Diagnostics/Logger.cs
@@ -0,0 +1,89 @@
+﻿// Logger.cs
+//
+// Copyright (C) 2013 Fabrício Godoy
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Diagnostics;
+
+namespace SklLib.Diagnostics
+{
+    public class Logger
+    {
+        #region Fields
+
+        const int EVENT_LOG_MAX_LENGTH = 15000;
+        EventLog eventLog;
+
+        #endregion
+
+        #region Constructors
+
+        public Logger(string source, string log)
+        {
+            eventLog = CreateEventlog(source, log);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void WriteEntry(string message, EventLogEntryType type, int eventId)
+        {
+            string[] msgArr;
+            if (message.Length > EVENT_LOG_MAX_LENGTH) {
+                msgArr = SklLib.Strings.Split(message, EVENT_LOG_MAX_LENGTH);
+            }
+            else
+                msgArr = new string[] { message };
+
+            foreach (string item in msgArr)
+                eventLog.WriteEntry(item, type, eventId);
+        }
+
+        private static EventLog CreateEventlog(string source, string log)
+        {
+            EventLog result = null;
+
+            try {
+                // PS> Remove-EventLog <logname>
+                if (EventLog.SourceExists(source)) {
+                    result = new EventLog { Source = source };
+                    if (result.Log != log) {
+                        EventLog.DeleteEventSource(source);
+                        result.Dispose();
+                        result = null;
+                    }
+                }
+
+                if (!EventLog.SourceExists(source)) {
+                    EventLog.CreateEventSource(source, log);
+                    result = new EventLog { Source = source, Log = log };
+                    result.WriteEntry("Event Log created",
+                        EventLogEntryType.Information, (int)EventId.EventLogCreated);
+                }
+            }
+            catch (Exception e) {
+                throw new Exception(string.Format(
+                    "Error creating EventLog (Source: {0} and Log: {1})", source, log), e);
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
